430cb6a9f95e11c9f8add8a91c3e70600ee3c1d4
Hubert Rudzik <253993@student.pwr.edu.pl>
2022-01-13 15:47:13 +0100

    Ending

--- Hacking.c
+++ Hacking.c
@@ -11,12 +11,12 @@
 #define PRODUCER_THREADS 3
 #define CONSUMER_THREAD 1
 
-FILE *passwords,*dictionary;
+FILE *passwords,*dictionary1,*dictionary2,*dictionary3;
 char emails[1000][64];
 char passwords_to_hack[1000][32];
 char passwords_hacked[1000][32];
 int k=0;
-pthread_mutex_t mutex1;
+pthread_mutex_t mutex1,mutex_dir1,mutex_dir2,mutex_dir3;
 pthread_cond_t cv1;
 struct Info
 {
@@ -46,78 +46,98 @@ void MD5_digest(char argv[],unsigned char tab[])
 void sig()
 {
     printf("\n");
-    for(int i=1;i<k;i++)
+    for(int i=0;i<k;i++)
     {
-        printf("%d. Email:%s, Password:%s\n",i,emails[i],passwords_hacked[i]);
+        printf("%d. Email:%s, Password:%s\n",i+1,emails[i],passwords_hacked[i]);
     }
 }
 void* Producer0(void* arg)
 {
+    int repeats=0;
+    int num=0;   
     while(1)
     {
-        char *example_password;
         unsigned char tab[16];
-        char password_string[32];
-        char buff1[33];
-        char buff2[33];
-        //
-       /* if(feof(dictionary)){
-            while(1)
-            {
-                int num=0;
+        char password_string[32],buff1[33],buff2[33];
+        char *example_password=(char*)malloc(sizeof(char)*32);
 
-            }
-        }*/
-        example_password=malloc(sizeof(char)*32);
-        fscanf(dictionary,"%s",example_password);
+        if(feof(dictionary1))
+        {
+            rewind(dictionary1);
+            repeats++;
+        }
+        pthread_mutex_lock(&mutex_dir1);
+            fscanf(dictionary1,"%s",example_password);
+        pthread_mutex_unlock(&mutex_dir1);
+
+        if(repeats!=0){
+            char buff[10];
+            sprintf(buff,"%d",num);
+            strcat(example_password,buff);
+            num++;
+        }
         MD5_digest(example_password,tab);
         for (int i = 0; i < 16; i++){
             sprintf(&password_string[i*2], "%02x", (unsigned int)tab[i]);
         }
+        pthread_mutex_lock(&mutex1);
         for (int i = 0; i < 32; i++)
         {
             buff1[i]=passwords_to_hack[k][i];
             buff2[i]=password_string[i];
         }
-        pthread_mutex_lock(&mutex1);
         if(strcmp(buff1,buff2)==0){
-            rewind(dictionary);
             strcpy(pass.passed_email,emails[k]);
             strcpy(pass.passed_password,example_password);
             k++;
+            repeats=0;
             pthread_cond_signal(&cv1);
         }
-        pthread_mutex_unlock(&mutex1);
         free(example_password);
+        pthread_mutex_unlock(&mutex1);
     }
 }
 void* Producer1(void* arg)
 {
-    while(!feof(dictionary))
+    int repeats=0;
+    int num=0;    
+    while(1)
     {
-        char *example_password;
         unsigned char tab[16];
-        char password_string[32];
-        char buff1[33];
-        char buff2[33];
-        example_password=malloc(sizeof(char)*32);
-        fscanf(dictionary,"%s",example_password);
+        char password_string[32],buff1[33],buff2[33];
+        char *example_password=(char*)malloc(sizeof(char)*32);
+
+        if(feof(dictionary2))
+        {
+            rewind(dictionary2);
+            repeats++;
+        }
+        pthread_mutex_lock(&mutex_dir2);
+            fscanf(dictionary2,"%s",example_password);
+        pthread_mutex_unlock(&mutex_dir2);
+
         example_password[0]=toupper(example_password[0]);
+        if(repeats!=0){
+            char buff[10];
+            sprintf(buff,"%d",num);
+            strcat(example_password,buff);
+            num++;
+        }
         MD5_digest(example_password,tab);
         for (int i = 0; i < 16; i++){
             sprintf(&password_string[i*2], "%02x", (unsigned int)tab[i]);
         }
+        pthread_mutex_lock(&mutex1);
         for (int i = 0; i < 32; i++)
         {
             buff1[i]=passwords_to_hack[k][i];
             buff2[i]=password_string[i];
         }
-        pthread_mutex_lock(&mutex1);
         if(strcmp(buff1,buff2)==0){
-            rewind(dictionary);
             strcpy(pass.passed_email,emails[k]);
             strcpy(pass.passed_password,example_password);
             k++;
+            repeats=0;
             pthread_cond_signal(&cv1);
         }
         pthread_mutex_unlock(&mutex1);
@@ -126,37 +146,50 @@ void* Producer1(void* arg)
 }
 void* Producer2(void* arg)
 {
-    while(!feof(dictionary))
+    int repeats=0;
+    int num=0;
+    while(1)
     {
-        char *example_password;
         unsigned char tab[16];
-        char password_string[32];
-        char buff1[33];
-        char buff2[33];
-        example_password=malloc(sizeof(char)*32);
-        fscanf(dictionary,"%s",example_password);
+        char password_string[32],buff1[33],buff2[33];
+        char *example_password=(char*)malloc(sizeof(char)*32);
+
+        if(feof(dictionary3))
+        {
+            rewind(dictionary3);
+            repeats++;
+            num++;
+        }
+        pthread_mutex_lock(&mutex_dir3);
+        fscanf(dictionary3,"%s",example_password);
+        pthread_mutex_unlock(&mutex_dir3);
         for (int i = 0; i < 32; i++){
             example_password[i]=toupper(example_password[i]);
+        }
+        if(repeats!=0){
+            char buff[10];
+            sprintf(buff,"%d",num);
+            strcat(example_password,buff);
         }
         MD5_digest(example_password,tab);
         for (int i = 0; i < 16; i++){
             sprintf(&password_string[i*2], "%02x", (unsigned int)tab[i]);
         }
+        pthread_mutex_lock(&mutex1);
         for (int i = 0; i < 32; i++)
         {
             buff1[i]=passwords_to_hack[k][i];
             buff2[i]=password_string[i];
         }
-        pthread_mutex_lock(&mutex1);
         if(strcmp(buff1,buff2)==0){
-            rewind(dictionary);
             strcpy(pass.passed_email,emails[k]);
             strcpy(pass.passed_password,example_password);
             k++;
+            repeats=0;
             pthread_cond_signal(&cv1);
         }
-        pthread_mutex_unlock(&mutex1); 
         free(example_password);
+        pthread_mutex_unlock(&mutex1); 
     }
 }
 void* Consumer(void* arg)
@@ -169,22 +202,29 @@ void* Consumer(void* arg)
         if(iterator!=k){
             printf("email:%s password:%s\n",pass.passed_email,pass.passed_password);
             strcpy(passwords_hacked[iterator],pass.passed_password);
+            rewind(dictionary1);
+            rewind(dictionary2);
+            rewind(dictionary3);
             iterator=k;
         }
         else
-            pthread_cond_wait(&cv1, &mutex1); //oczekuje zwalnia a potem znowu zatrzaskuje;
+            pthread_cond_wait(&cv1, &mutex1); 
     }
     pthread_mutex_unlock(&mutex1);
 }
 
 int main(int argc, char* argv[])
 {
+    char* new_passwords;
     long i=0;
     if((passwords=fopen(argv[1],"r"))==NULL){
                 printf("ERROR1\n");
                 return 0;
     }
-    if((dictionary=fopen(argv[2],"r"))==NULL)
+    dictionary1=fopen(argv[2],"r");
+    dictionary2=fopen(argv[2],"r");
+    dictionary3=fopen(argv[2],"r");
+    if((dictionary1==NULL)||(dictionary2==NULL)||(dictionary3==NULL))
     {
         printf("ERROR2\n");
         return 0;
@@ -197,12 +237,33 @@ int main(int argc, char* argv[])
     }
     pthread_t thread[PRODUCER_THREADS+CONSUMER_THREAD];
     pthread_mutex_init(&mutex1, NULL);
-    pthread_mutex_init(&mutex2, NULL);
+    pthread_mutex_init(&mutex_dir1, NULL);
+    pthread_mutex_init(&mutex_dir2, NULL);
+    pthread_mutex_init(&mutex_dir3, NULL);
     pthread_cond_init (&cv1, NULL);
     pthread_create(&thread[3],NULL,Consumer,NULL); 
     pthread_create(&thread[0],NULL,Producer0,NULL); 
     pthread_create(&thread[1],NULL,Producer1,NULL); 
     pthread_create(&thread[2],NULL,Producer2,NULL); 
+    while(1){
+        scanf("%s",new_passwords);
+        if((passwords=fopen(new_passwords,"r"))==NULL){
+            printf("ERROR1\n");
+            return 0;
+        }
+        pthread_mutex_lock(&mutex_dir1);
+        pthread_mutex_lock(&mutex_dir2);
+        pthread_mutex_lock(&mutex_dir3);
+        while(!feof(passwords))
+        {
+            fscanf(passwords,"%s",emails[i]);
+            fscanf(passwords,"%s",passwords_to_hack[i]);
+            i++;
+        }
+        pthread_mutex_unlock(&mutex_dir1);
+        pthread_mutex_unlock(&mutex_dir2);
+        pthread_mutex_unlock(&mutex_dir3);
+    }
     for(int i=0;i<PRODUCER_THREADS+CONSUMER_THREAD;i++)
     {
         pthread_join(thread[i],NULL);
